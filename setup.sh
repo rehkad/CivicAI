3c90127ce910be156a9bed9379e7e0f70c3622f8
rehkad <100399936+rehkad@users.noreply.github.com>
2025-06-25 17:42:49 -0700

    Add flake8 and make setup script tolerant to failures

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+# Allow installs to fail in offline environments
+
+
 # Upgrade pip
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip || true
 
 # If a local wheels directory exists, install from there
 if [ -d "wheels" ]; then
-    python -m pip install --no-index --find-links wheels -r requirements.txt
+    python -m pip install --no-index --find-links wheels -r requirements.txt || true
 else
-    python -m pip install -r requirements.txt
+    python -m pip install -r requirements.txt || true
 fi
